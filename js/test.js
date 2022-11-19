01a5824d9f97b496a69d89c820681abd293d49bf
userzahar <110730909+userzahar@users.noreply.github.com>
2022-11-19 14:31:30 +0200

    ок

--- js/test.js
+++ js/test.js
@@ -1,6 +1,13 @@
-const pricePerDroid =800;
-const orderedQuantity =6;
-const deliveryFee = 50;
-const totalPrice = pricePerDroid*orderedQuantity+deliveryFee;
-const message = 'You ordered droids worth ' + totalPrice + ' credits. Delivery (' + deliveryFee + ' credits) is included in total price.';
-console.log(message);
+// const pricePerDroid =800;
+// const orderedQuantity =6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid*orderedQuantity+deliveryFee;
+// const message = 'You ordered droids worth ' + totalPrice + ' credits. Delivery (' + deliveryFee + ' credits) is included in total price.';
+// console.log(message);
+let clientCounter = 18;
+const maxClients = 25;
+
+while (clientCounter < maxClients) {
+  console.log(clientCounter);
+  clientCounter += 1;
+};

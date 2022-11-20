30119aa3336c1a230c41466d19704ca720e22ab1
userzahar <110730909+userzahar@users.noreply.github.com>
2022-11-20 14:03:14 +0200

    s

--- js/study.js
+++ js/study.js
@@ -1,15 +1,76 @@
-//Замінити всі розширення файлів з .css на .js
-//const files = ['index.css', 'script.js', 'style.css', 'step-types.json', 'firebase.css'];
-// створити констант для массиву
-// створити цикл
-// замінити з допомогою replace() 
-
-const files = ['index.css', 'script.js', 'style.css', 'step-types.json', 'firebase.css'];
-// for (let i = 0; i < files.length; i += 1) {
-//     // console.log(files[i]);
-//     files[i] = files[i].replace(".css", ".js");
+// function getExtremeElements(array) {
+//     // Change code below this line
+//   let newArray = array.splice(1, array.length-2);
+//     return array;
+//     // Change code above this line
 // }
-// console.log(files);
+// const lipton = ['apple', 'peach', 'pear', 'banana'];
+//   console.log(getExtremeElements(lipton))
+// let message;
+// function calculateEngravingPrice(message, pricePerWord) {
+//     let widthMessage = message.split(" ").length;
+//     let totalPrice = widthMessage * pricePerWord;
+//     return totalPrice;
+// }
+// console.log(calculateEngravingPrice('Qlb lj legb', 20));
+// message = 'Qlb lj legb'
+// console.log(message.split(" ").length)
+// function slugify(title) {
+//     // Change code below this line
+//     let slug = title.toLowerCase().split(" ").join('-');
+//     return slug;
+//     // Change code above this line
+// }
+// console.log(slugify("репорт жирні тролі"));
 
-const filesTojs = files.join(" ").replaceAll(".css", ".js").split(" ");
-// console.log(filesTojs);
+// function calculateTotal(number) {
+//     // Change code below this line
+//     for (let i = 0; i < number; i += 1) {
+//         let sum = 10 + i;
+//         console.log(sum);
+//     }
+//     return sum;
+
+   
+//      // Change code above this line
+// }
+// console.log(calculateTotal(10));
+// let sum = 12;
+// for (let i = 0; i < number; i += 1) {
+//     let sum = sum + i;
+// }
+// console.log(sum);
+// function calculateTotal(number) {
+//     // Change code below this line
+//     let newnumber;
+//     let collection;
+//     for (let i = 0; i < number; i += 1) {
+//         newnumber = number - i;
+//         // console.log(number);
+//         collection = newnumber + number;
+//     }
+//     return collection;
+   
+//      // Change code above this line
+// }
+// console.log(calculateTotal(10))
+// const target = 3;
+// let sum = 0;
+
+// for (let i = 0; i <= target; i += 1) {
+//   sum += i;
+// }
+
+// console.log(sum);
+function findLongestWord(string) {
+    // Change code below this line
+    let splitString = string.split(" ")
+    fo
+    }
+  
+  
+  
+    // Change code above this line
+}
+  
+// 'zбвко дороа ааааа сссссссс';

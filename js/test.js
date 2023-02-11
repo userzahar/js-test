9e39159993c4a73553db45f571ad51f1b8840beb
userzahar <110730909+userzahar@users.noreply.github.com>
2023-02-11 17:29:50 +0200

    jy

--- js/test.js
+++ js/test.js
@@ -53,71 +53,79 @@
 //     })
 //     .catch(console.log);
 // }
+// ! скролллллллллллллллллллллллллллллллллллл
+// let options = {
+//   root: null,
+//   rootMargin: "300px",
+//   threshold: 0,
+// };
+// let callback = (entries, observer) => {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       page += 1;
+//       getSearch(page).then((res) => {
+//         renderCard(res.results);
+//         if (res.info.pages === page) {
+//           buttonLoadMore.hidden = true;
+//           observer.unobserve(target);
+//           return;
+//         }
+//       });
+//     }
+//   });
+// };
+// let observer = new IntersectionObserver(callback, options);
+// let target = document.querySelector(".js-guard");
+// // +;;;;;;;;;;;;;
+// const gallery = document.querySelector(".gallery");
+// const buttonLoadMore = document.querySelector(".load-more");
+// let page = 1;
+// buttonLoadMore.addEventListener("click", onLoadMoreClick);
 
-let options = {
-  root: null,
-  rootMargin: "300px",
-  threshold: 0,
-};
-let callback = (entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      page += 1;
-      getSearch(page).then((res) => {
-        renderCard(res.results);
-        if (res.info.pages === page) {
-          buttonLoadMore.hidden = true;
-          observer.unobserve(target);
-          return;
-        }
-      });
-    }
-  });
-};
-let observer = new IntersectionObserver(callback, options);
-let target = document.querySelector(".js-guard");
-// +;;;;;;;;;;;;;
-const gallery = document.querySelector(".gallery");
-const buttonLoadMore = document.querySelector(".load-more");
-let page = 1;
-buttonLoadMore.addEventListener("click", onLoadMoreClick);
+// async function getSearch(page = 1) {
+//   const response = await fetch(
+//     `https://rickandmortyapi.com/api/character/?page=${page}`
+//   );
+//   if (!response.ok) {
+//     throw new Error(response.statusText);
+//   }
+//   return response.json();
+// }
 
-async function getSearch(page = 1) {
-  const response = await fetch(
-    `https://rickandmortyapi.com/api/character/?page=${page}`
-  );
-  if (!response.ok) {
-    throw new Error(response.statusText);
-  }
-  return response.json();
+// getSearch().then((res) => {
+//   renderCard(res.results);
+//   buttonLoadMore.hidden = false;
+// });
+
+// function renderCard(arrObj) {
+//   console.log(arrObj);
+//   const card = arrObj
+//     .map(({ name, image }) => {
+//       return `<div>
+//     <h2>${name}
+//     </h2>
+//     <img src="${image}" alt="${name}"></img>
+// </div>`;
+//     })
+//     .join("");
+//   gallery.insertAdjacentHTML("beforeend", card);
+//   observer.observe(target);
+// }
+
+// function onLoadMoreClick() {
+//   page += 1;
+//   getSearch(page).then((res) => {
+//     if (res.info.pages === page) {
+//       buttonLoadMore.hidden = true;
+//     }
+//     renderCard(res.results);
+//   });
+// }
+
+// ! геолокаааааааааааааааааааааааааація
+// через weather api
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
 }
-
-getSearch().then((res) => {
-  renderCard(res.results);
-  buttonLoadMore.hidden = false;
-});
-
-function renderCard(arrObj) {
-  console.log(arrObj);
-  const card = arrObj
-    .map(({ name, image }) => {
-      return `<div>
-    <h2>${name}
-    </h2>
-    <img src="${image}" alt="${name}"></img>
-</div>`;
-    })
-    .join("");
-  gallery.insertAdjacentHTML("beforeend", card);
-  observer.observe(target);
-}
-
-function onLoadMoreClick() {
-  page += 1;
-  getSearch(page).then((res) => {
-    if (res.info.pages === page) {
-      buttonLoadMore.hidden = true;
-    }
-    renderCard(res.results);
-  });
-}
+// ! завданняяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяя
+// https://docs.google.com/presentation/d/1alj1TPBpcqiRLg1EV74cYD6dFts-TPMy/edit#slide=id.p6

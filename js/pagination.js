f76af57eb5003f63ab041314a97863a556d56bf6
userzahar <110730909+userzahar@users.noreply.github.com>
2023-02-14 20:53:10 +0200

    dcssdc

---
+++ js/pagination.js
@@ -0,0 +1,6 @@
+import Pagination from "/tui-pagination";
+import "tui-pagination/dist/tui-pagination.css";
+const container = document.getElementById("pagination");
+console.log("🚀 ~ container", container);
+
+const pagination = new Pagination(container);

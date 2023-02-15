e36cf7c4936a4a7e164e1f73efd070092238a7d7
userzahar <110730909+userzahar@users.noreply.github.com>
2023-02-15 17:25:35 +0200

    стилізував тумблєр

--- js/pagination.js
+++ js/pagination.js
@@ -1,32 +1,14 @@
-import Pagination from "/tui-pagination";
-import "tui-pagination/dist/tui-pagination.css";
-const container = document.getElementById("pagination");
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+  return data;
+}
 
-const options = {
-  totalItems: 10,
-  itemsPerPage: 10,
-  visiblePages: 10,
-  page: 1,
-  centerAlign: false,
-  firstItemClassName: "tui-first-child",
-  lastItemClassName: "tui-last-child",
-  template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage:
-      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-    moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      "</a>",
-    disabledMoveButton:
-      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      "</span>",
-    moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-      '<span class="tui-ico-ellip">...</span>' +
-      "</a>",
-  },
-};
-
-const pagination = new Pagination("pagination", options);
+async function main() {
+  const postsData = await getData();
+  const currentPage = 1;
+  let rows = 10;
+  function displayList() {}
+  function displayPagination() {}
+  function displayPaginationBtn() {}
+}
